be4cd9a98a64b327fe81bf36108106a487092625
ZapcoMan <3507557934@qq.com>
2025-08-02 21:28:19 +0800

    refactor(springboot):调整 CourseService 接口格式
    - 在 CourseService 接口中添加了 BaseServie 的导入- 优化了接口定义的格式，使其更加规范

--- springboot/src/main/java/com/example/modules/system/service/CourseService.java
+++ springboot/src/main/java/com/example/modules/system/service/CourseService.java
@@ -1,11 +1,12 @@
 package com.example.modules.system.service;
 
 
+import com.example.core.service.BaseService;
 import com.example.modules.system.entity.Course;
 
 import java.util.List;
 
-public interface CourseService extends BaseService<Course, Long>{
+public interface CourseService extends BaseService<Course, Long> {
     Course getById(Long id);
     List<Course> list();
     void create(Course c);
