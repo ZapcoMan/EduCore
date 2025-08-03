3e9ffb72a16df5ba2bc2b8a9811ec13507e46dd8
codervibe <3217277135@qq.com>
2025-08-03 15:11:41 +0800

    refactor(redis): 优化 RedisUtil 类
    - 指定 stringRedisTemplate 作为 RedisTemplate 的资源
    - 简化 exists 方法的实现

--- springboot/src/main/java/com/example/common/utils/RedisUtil.java
+++ springboot/src/main/java/com/example/common/utils/RedisUtil.java
@@ -10,15 +10,16 @@ import java.util.concurrent.TimeUnit;
 @Component
 public class RedisUtil {
 
-    @Resource
+    @Resource(name = "stringRedisTemplate")
     private StringRedisTemplate redisTemplate;
+
 
     public void set(String key, String value, long timeout, TimeUnit unit) {
         redisTemplate.opsForValue().set(key, value, timeout, unit);
     }
 
     public boolean exists(String key) {
-        return Boolean.TRUE.equals(redisTemplate.hasKey(key));
+        return redisTemplate.hasKey(key);
     }
 
     public void delete(String key) {
