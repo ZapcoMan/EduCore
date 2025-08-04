08c1be3bba67dd496afe0fde0c0d552827234815
codervibe <3217277135@qq.com>
2025-08-04 19:49:55 +0800

    refactor(exception): 优化 CustomerException 类
    - 添加了类的详细注释，说明了类的用途
    - 为每个构造函数添加了参数说明，提高了代码的可读性
    - 删除了多余的空行，优化了代码结构

--- springboot/src/main/java/com/example/common/exception/CustomerException.java
+++ springboot/src/main/java/com/example/common/exception/CustomerException.java
@@ -5,9 +5,9 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.extern.slf4j.Slf4j;
 
-
 /**
  * 自定义业务异常，包含异常代码与异常消息
+ * 用于封装业务逻辑中出现的异常情况，提供统一的异常处理机制
  */
 @Slf4j
 @EqualsAndHashCode(callSuper = true)
@@ -17,18 +17,32 @@ public class CustomerException extends RuntimeException {
     private String code;
     private String msg;
 
+    /**
+     * 构造函数，使用指定的错误代码和消息创建异常
+     * @param code 错误代码
+     * @param msg 错误消息
+     */
     public CustomerException(String code, String msg) {
         super(msg);
         this.code = code;
         this.msg = msg;
     }
 
+    /**
+     * 构造函数，使用指定的消息创建异常，错误代码使用默认的未知错误代码
+     * @param msg 错误消息
+     */
     public CustomerException(String msg) {
         super(msg);
         this.code = String.valueOf(ResultCodeEnum.UNKNOWN_ERROR.getCode());
         this.msg = msg;
     }
 
+    /**
+     * 构造函数，使用指定的错误代码和异常对象创建异常
+     * @param code 错误代码
+     * @param e 异常对象
+     */
     public CustomerException(String code, Exception e) {
         super(e);
         this.code = code;
