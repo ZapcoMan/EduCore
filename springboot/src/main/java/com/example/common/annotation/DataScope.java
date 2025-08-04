5a8eb3d9596c4a7a90890b44cfcece1e8ca2ebe5
codervibe <3217277135@qq.com>
2025-08-04 16:47:11 +0800

    feat(common): 添加 DataScope 注解文档和方法说明
    - 为 DataScope 注解添加文档注释，说明其用途和使用场景- 为 deptAlias 方法添加注释，解释其功能和返回值

--- springboot/src/main/java/com/example/common/annotation/DataScope.java
+++ springboot/src/main/java/com/example/common/annotation/DataScope.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 数据权限注解，用于控制用户数据访问范围
+ * 该注解只能用于方法上，在运行时保留，配合AOP实现数据权限过滤功能
+ */
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DataScope {
@@ -13,6 +17,9 @@ public @interface DataScope {
      */
     String userAlias() default "";
 
+    /**
+     * 部门表别名，用于 SQL 拼接
+     * @return 部门表别名
+     */
     String deptAlias() default "";
 }
-
